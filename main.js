d0779af26703e0cb70bf02e3e53f8beb903d9faf
dangVuDinh <139780574+dangNguyenDinh@users.noreply.github.com>
2024-02-19 22:28:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 setInterval(()=>{
     document.querySelectorAll(".gizmo-shadow-stroke").forEach((i)=>{
         if(i.textContent.includes("ChatGPT")){
-            i.innerHTML = `<img src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-6/426570308_309246865491307_6628576440809608514_n.jpg?_nc_cat=111&amp;ccb=1-7&amp;_nc_sid=efb6e6&amp;_nc_eui2=AeH4YMBN95tOutGrjwMiNVpq0jOD3V0rZ6HSM4PdXStnofklWJSx6rpy1zA-Gu4AtPQT4C0bzbGHroP_70acGa3g&amp;_nc_ohc=Mj7IosiI1MsAX9QuxWh&amp;_nc_ht=scontent.fsgn2-6.fna&amp;oh=00_AfAdYbFAjfLmByPOWshJf9LNLk2D0P6uyDjnmLUwymkBFQ&amp;oe=65D8BB47" alt="#">`
+            i.innerHTML = `<img src="https://scontent.fhan14-2.fna.fbcdn.net/v/t1.18169-9/536793_105670412903117_46630590_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=300f58&_nc_eui2=AeGjKJP1GMTNVzr1UKOwhwPsmOwehvekhIqY7B6G96SEih50mdG7S0wJn1ICgN8m54iItaJ3SHLzcxrTTbk0ioUp&_nc_ohc=nlVmlS-ljdYAX8J_H_O&_nc_ht=scontent.fhan14-2.fna&oh=00_AfCwhg7bm-i-nsmVacWte4PCQILn6XP8qZ1WEHg15YvPWw&oe=65FAFCDC" alt="#">`
         }
     })
     var a = document.querySelectorAll(".font-semibold")
     for(var i=0;i<a.length;i++){
         if(a[i].textContent.includes("ChatGPT")){
-            a[i].textContent = "Luong Hong Ngoc";
+            a[i].textContent = "Hinata chan";
         }
     }
 }, 200)
