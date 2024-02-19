0a84d8dd24c37ed6478765e30844ce733031ed81
dangVuDinh <139780574+dangNguyenDinh@users.noreply.github.com>
2024-02-19 11:40:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 setInterval(()=>{
     document.querySelectorAll(".gizmo-shadow-stroke").forEach((i)=>{
         if(i.textContent.includes("ChatGPT")){
-            i.innerHTML = `<img src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-6/426570308_309246865491307_6628576440809608514_n.jpg?_nc_cat=111&amp;ccb=1-7&amp;_nc_sid=efb6e6&amp;_nc_eui2=AeH4YMBN95tOutGrjwMiNVpq0jOD3V0rZ6HSM4PdXStnofklWJSx6rpy1zA-Gu4AtPQT4C0bzbGHroP_70acGa3g&amp;_nc_ohc=Mj7IosiI1MsAX9QuxWh&amp;_nc_ht=scontent.fsgn2-6.fna&amp;oh=00_AfAdYbFAjfLmByPOWshJf9LNLk2D0P6uyDjnmLUwymkBFQ&amp;oe=65D8BB47" alt="#">`
+            i.innerHTML = `<img src="https://scontent.fhan20-1.fna.fbcdn.net/v/t39.30808-6/410566437_1047399673264696_8103756764118983425_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeGxZeibdk78icEcBfXBQGWNti9ywnsQINe2L3LCexAg1x6-97ey4m3CE5l_2_dX9T_ObIByMDt7vOYUsgg_sYVO&_nc_ohc=aNX_Up9kHUoAX9JC_Mo&_nc_ht=scontent.fhan20-1.fna&oh=00_AfAxHoOh1XqLmMpT6DKycyamC8or0E5hwP_jp-F_SSy__Q&oe=65D8116B" alt="#">`
         }
     })
     var a = document.querySelectorAll(".font-semibold")
     for(var i=0;i<a.length;i++){
         if(a[i].textContent.includes("ChatGPT")){
-            a[i].textContent = "Luong Hong Ngoc";
+            a[i].textContent = "Minh Hương";
         }
     }
 }, 200)
