1e687eaea7685279d760afeea2c314805cec150b
Uchihahahaha <139780574+dangNguyenDinh@users.noreply.github.com>
2024-05-14 21:44:58 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,45 +3,56 @@ document.getElementById("__next").style.fontFamily = "Tahoma";
 //name  
 console.log("start");
 var canExec = 0;
-//nút newchat
-var a = document.querySelectorAll("div");
-var allDiv = [];
-a.forEach((i) => {
-    if (i.textContent.includes("New chat")) {
-        allDiv.push(i);
-    }
-});
-allDiv[10].addEventListener('click', () => {
-    console.log("new chat actived");
-    canExec = 0;
-    window.location.href = "https://chat.openai.com/";
-});
+
+
 //enter
 var hasLetter = false;
 document.querySelector("textarea").addEventListener('keydown', (e) => {
     console.log(canExec);
-
     if (e.key == "Enter") {
         console.log(canExec);
         if (canExec >= 2) {
+            //nút newchat
+            var a = document.querySelectorAll("div");
+            var allDiv = [];
+            a.forEach((i) => {
+                if (i.textContent.includes("Đoạn chat mới")) {
+                    allDiv.push(i);
+                }
+            });
+            allDiv[10].addEventListener('click', () => {
+                console.log("new chat actived");
+                canExec = 0;
+                window.location.href = "https://chatgpt.com/";
+            });
+
             setInterval(() => {
-                //background
-                document.querySelectorAll(".h-full")[10].style.backgroundImage = "url('https://i.pinimg.com/originals/5d/7c/c4/5d7cc4c44e0f6fd0c1345bc357c3d14c.jpg')";
-                document.querySelectorAll(".h-full")[11].style.backgroundSize = "cover";
-                document.querySelectorAll(".sticky")[1].style.backgroundImage = "url(https://i.pinimg.com/originals/5d/7c/c4/5d7cc4c44e0f6fd0c1345bc357c3d14c.jpg)";
+                //background 
+                document.querySelector('[role="presentation"]').style.backgroundImage = "url('https://i.pinimg.com/originals/5d/7c/c4/5d7cc4c44e0f6fd0c1345bc357c3d14c.jpg')";
+                document.querySelectorAll('.bg-token-main-surface-primary')[2].style.backgroundImage = "url('https://i.pinimg.com/originals/5d/7c/c4/5d7cc4c44e0f6fd0c1345bc357c3d14c.jpg')";
+                document.querySelectorAll('.bg-token-main-surface-primary')[1].style.backgroundImage = "url('https://i.pinimg.com/originals/5d/7c/c4/5d7cc4c44e0f6fd0c1345bc357c3d14c.jpg')";
+
+                //message color
+                var messages = document.querySelectorAll(".flex-col .gap-1");
+                for (let i = 0; i < messages.length; i++) {
+                    messages[i].style.backgroundColor = "#FDFFF5";
+                    messages[i].style.borderRadius = "10px";
+                    messages[i].style.padding = "10px 15px";
+                    messages[i].style.boxShadow = "5px 5px 20px darkgray";
+                }
 
                 //ảnh gpt
                 var iconsm = document.querySelectorAll(".icon-sm");
                 for (var i = 0; i < iconsm.length; i++) {
-                    if(iconsm[i].parentNode.textContent.includes("ChatGPT")){
+                    if (iconsm[i].parentNode.textContent.includes("ChatGPT")) {
                         iconsm[i].parentNode.parentNode.innerHTML = `<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnRyY6iVV7oCsNl-Jqz858LYty_oiafLQ9zAyjW1PIqQ&s" alt="#">`;
                     }
                 }
 
                 //ảnh người dùng
                 var fontsemibold = document.querySelectorAll(".font-semibold");
-                for(let i=0;i<fontsemibold.length;i++){
-                    if(fontsemibold[i].textContent.includes("You")){
+                for (let i = 0; i < fontsemibold.length; i++) {
+                    if (fontsemibold[i].textContent.includes("B")) {
                         fontsemibold[i].parentNode.parentNode.querySelector("img").src = "https://pm1.aminoapps.com/6736/3fbaea905ca7aa1b28498ac082f440fe902af8a9v2_hq.jpg"
                     }
                 }
@@ -51,7 +62,7 @@ document.querySelector("textarea").addEventListener('keydown', (e) => {
                     if (a1[i].textContent.includes("ChatGPT")) {
                         a1[i].textContent = "Rin";
                     }
-                    if (a1[i].textContent.includes("You")) {
+                    if (a1[i].textContent.includes("B")) {
                         a1[i].textContent = "Obito";
                     }
                 }
@@ -67,19 +78,19 @@ document.querySelector("textarea").addEventListener('keydown', (e) => {
 
 
 
-    // var background = document.createElement("div");
-    // var img = document.createElement("img");
-    // img.src = "https://i.pinimg.com/736x/66/f5/cf/66f5cf306d1690f29cc465d4e69ecc7f.jpg";
-    // img.alt = "#";
-    // img.style.width = "100vw";
-    // background.appendChild(img);
-    // background.style.opacity = '0.3';
-    // background.style.position = 'fixed';
-    // background.style.width = '100vw';
-    // background.style.height = '100vh';
-    // background.style.top = '0';
-    // background.style.zIndex = '-1';
-    // document.querySelector("body").appendChild(background);
+// var background = document.createElement("div");
+// var img = document.createElement("img");
+// img.src = "https://i.pinimg.com/736x/66/f5/cf/66f5cf306d1690f29cc465d4e69ecc7f.jpg";
+// img.alt = "#";
+// img.style.width = "100vw";
+// background.appendChild(img);
+// background.style.opacity = '0.3';
+// background.style.position = 'fixed';
+// background.style.width = '100vw';
+// background.style.height = '100vh';
+// background.style.top = '0';
+// background.style.zIndex = '-1';
+// document.querySelector("body").appendChild(background);
 
 // const e = new KeyboardEvent('keydown', {
 //     bubbles: true,
